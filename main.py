c7b29ac852c0aa7e75e6834ce173776dcac79475
LexaHood <allpetr@list.ru>
2020-09-25 12:22:33 +0300

    добавил комменты и немного красоты в файл вывода

--- main.py
+++ main.py
@@ -1,31 +1,30 @@
-import socket
 import threading
 import time
 import subprocess
 from threading import Thread
 import packege_class
 
-start_time = time.time()
+start_time = time.time() #запомниаем когда запустилась программа
 
-mac_printer_list = [
+mac_printer_list = [ #Список мак адресов производителей принтеров
 '3c:2a:f4', #Brother
 '00:17:c8', #Koycera
 'f4:30:b9'] #HP
 
-length_mac_lsit = len(mac_printer_list)
-list_printers = []
+length_mac_lsit = len(mac_printer_list) #узнаем колличесво мак адресов производителей
+list_printers = [] #создаем лист под найденные адреса принтеров в сети
 
-def delimiter_mac_address (ip_adress):
-    result = subprocess.run(['arp', ip_adress], stdout=subprocess.PIPE)
-    tmp = result.stdout.decode('utf-8')
-    index_first = tmp.find("ether") + 8
-    index_last = tmp.find(" ", index_first)
-    return (tmp[index_first:index_last])
+def delimiter_mac_address (ip_adress): #эта функция делает арп по айпишнику и выделяет мак адрес из выода консоли
+    result = subprocess.run(['arp', ip_adress], stdout=subprocess.PIPE) #записываем результат из терминала
+    tmp = result.stdout.decode('utf-8') #для полной уверенности переведем строчку в юникод 
+    index_first = tmp.find("ether") + 8 #ищем где же начинается мак адрес
+    index_last = tmp.find(" ", index_first) #ищем где заканчивается 
+    return (tmp[index_first:index_last]) 
 
-def check_mac (mac_adress):
+def check_mac (mac_adress): #проверяем, полученный мак адрес это принтер
     for i in range (length_mac_lsit):
         tmp = mac_adress[0 : 8]
-        if tmp.find(mac_printer_list[i]) != -1:
+        if tmp.find(mac_printer_list[i]) != -1: #если в получаном адресе есть код указанного производителя вернуть правду
             return True
     pass
     return False
@@ -34,19 +33,19 @@ def main ():
 
     ip = '10.1.1.'
 
-    file = open("printer list.conf", "w", 1 , "UTF-8")
+    file = open("printer list.conf", "w", 1 , "UTF-8") #открываем файл для записи
 
-    def thread_function(start_adress, end_adress):  
+    def thread_function(start_adress, end_adress): #основная функция всей обработки, скомпонована для более простым управлением потока
         for adress in range (start_adress, end_adress, 1):
             print ('check ip: ', ip, adress)
-            mac_adress = delimiter_mac_address(ip + str(adress))
-            res = check_mac(mac_adress)
+            mac_adress = delimiter_mac_address(ip + str(adress)) #получаем мак по заданному ip
+            res = check_mac(mac_adress) #проверяем принадлежит ли мак адрес нужному производителю
             if res == True:
-                list_printers.append(packege_class.net_packege(ip + str(adress), mac_adress))
+                list_printers.append(packege_class.net_packege(ip + str(adress), mac_adress)) #записываем айпи и мак адрес принтера в массив
             pass
         pass
     
-    def thread_launcher():
+    def thread_launcher(): #Запускаем и останавливаем потоки
         thread1 = Thread(target=thread_function, args=(1, 64))
         thread2 = Thread(target=thread_function, args=(64, 128))
         thread3 = Thread(target=thread_function, args=(128, 192))
@@ -65,20 +64,21 @@ def main ():
 
     thread_launcher()
 
-    size = len(list_printers)
-    print (size)
+    size = len(list_printers) 
+    #print (size)
     if size != 0:
-        for i in range (size):
+        for i in range (size): #записываем все найденные принтеры в файл
             list_printers[i].print_packege()
-            file.write(list_printers[i].get_string_to_write())
+            file.writelines(list_printers[i].get_string_to_write() + '\n')
             pass
         pass
     else: 
-        print ("printers not founded")
+        print ("printers not founded") #если list_printers пустой, значит мы ничего не нашли
+        print ("printers either are not present in the local network, or the poppy address is not entered in the search base")
 
 
     file.close()
     pass
 
 main()
-print("--- %s second ---" % (time.time() - start_time))
+print("--- %s second ---" % (time.time() - start_time)) #выводим время за которое выполнилась программа
